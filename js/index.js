c431ab0d479b76ff1d0e81ad09c05468e9f12e11
josewweee <josemb125@gmail.com>
2019-06-26 19:32:00 -0500

    agregando los input de datos personales

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//TOMAMOS LOS DATOS DEL USUARIO
+
+function OpenPersonalData(){
+    var formDatosPersonales = document.getElementById("datosPersonales");
+    var formVehiculo = document.getElementById("datosVehiculo");
+
+    formVehiculo.style.display = 'none';
+    formDatosPersonales.style.display = "block";
+}
